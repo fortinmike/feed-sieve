ad03c3dfc2c11ffdd93eb32d793795d7aaa78b20
Michaël Fortin <fortinmike@irradiated.net>
2025-09-03 20:21:27 -0400

    Implement cache

--- Logic/Cache.cs
+++ Logic/Cache.cs
@@ -5,15 +5,41 @@ public class Cache
     public Cache(DirectoryInfo directory)
     {
         _directory = directory;
+
+        if (!_directory.Exists)
+            _directory.Create();
     }
 
-    public string Get(string name, string hash)
+    public string? Get(string key, string hash)
     {
-        return null; // TODO: Implement!
+        var (subDir, valueFile, hashFile) = GetPaths(key);
+
+        if (!File.Exists(valueFile) || !File.Exists(hashFile))
+            return null;
+
+        var storedHash = File.ReadAllText(hashFile);
+        if (!string.Equals(storedHash, hash, StringComparison.Ordinal))
+            return null;
+
+        return File.ReadAllText(valueFile);
     }
 
-    public void Set(string name, string hash, string value)
+    public void Set(string key, string hash, string value)
     {
-        // TODO: Implement!
+        var (subDir, valueFile, hashFile) = GetPaths(key);
+        if (!subDir.Exists)
+            subDir.Create();
+
+        File.WriteAllText(valueFile, value);
+        File.WriteAllText(hashFile, hash);
+    }
+
+    private (DirectoryInfo subDir, string valueFile, string hashFile) GetPaths(string name)
+    {
+        var safeName = name.ToSafeFileName();
+        var subDir = new DirectoryInfo(Path.Combine(_directory.FullName, safeName));
+        var valueFile = Path.Combine(subDir.FullName, "value.txt");
+        var hashFile = Path.Combine(subDir.FullName, "hash.txt");
+        return (subDir, valueFile, hashFile);
     }
 }
