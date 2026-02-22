4ae40646bd54e079373a915d01f1ad910d459d9c
Michaël Fortin <fortinmike@irradiated.net>
2026-02-22 18:00:02 -0500

    Fix parsing of malformed RSS feeds with whitespace before the XML declaration

--- Controllers/FilterController.cs
+++ Controllers/FilterController.cs
@@ -124,7 +124,7 @@ public class FilterController : ControllerBase
             try
             {
                 // Modify the original RSS document by processing it with the loaded rules
-                var originalDocument = XDocument.Parse(originalRss);
+                var originalDocument = XDocument.Parse(originalRss.TrimStart());
                 var modifiedDocument = _processor.Process(originalDocument, rules, feedUrl);
                 var modifiedRss = modifiedDocument.ToString();
                 _cache.Set(feedUrl, hash, modifiedRss);
